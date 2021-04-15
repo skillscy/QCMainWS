7e7f0fc1e125825502c64bc6191012cbc52b609a
Regilan <mariairegilan@gmail.com>
2021-04-15 19:12:10 +0530

    Initial commit

---
+++ src/main/java/com/quartciphers/skillscy/QcMainWsApplication.java
@@ -0,0 +1,13 @@
+package com.quartciphers.skillscy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcMainWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QcMainWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quartciphers/skillscy/QcMainWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quartciphers.skillscy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcMainWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
