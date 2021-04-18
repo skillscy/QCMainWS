0f25fdf5dd6c322e737931c13ca717cde3078a92
Regilan <mariairegilan@gmail.com>
2021-04-18 21:32:18 +0530

    modified URL path

--- src/main/java/com/quartciphers/skillscy/QCMainWSController.java
+++ src/main/java/com/quartciphers/skillscy/QCMainWSController.java
@@ -19,14 +19,14 @@ public class QCMainWSController {
     @Autowired
     private QCMainWSService service;
 
-    @GetMapping(value = "/hello", produces = "application/json")
+    @GetMapping(value = "/api/hello", produces = "application/json")
     @ApiOperation(value = "A hello method which check health", nickname = "Hello method", notes = "This method returns the running status of the application along with the application URL, Swagger URL and other company related information", produces = "application/json", response = HealthStatus.class)
     public ResponseEntity<HealthStatus> helloToHealth() {
         HealthStatus healthStatus = new HealthStatus(ServletUriComponentsBuilder.fromCurrentContextPath().toUriString()); // creates HealthStatus object with necessary parameters
         return ResponseEntity.ok(healthStatus); // returns the response to the consumer
     }
 
-    @GetMapping(value = "/v1/youtube", produces = "application/json")
+    @GetMapping(value = "/api/v1/youtube", produces = "application/json")
     @ApiOperation(value = "This API will return the requested number of latest videos from the requested YouTube channel ID", produces = "application/json")
     public ResponseEntity<WebServiceCommonResponse> getLatestYouTubeVideoList(@RequestHeader("channel_id") String channelID, @RequestHeader(value = "count", required = false, defaultValue = "3") int count) {
         try {
